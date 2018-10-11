ae9c8de1b2a6399ecd489b4598e8188d3bafab6d
Jackie Pearce <jackiepearce83@hotmail.com>
2018-10-11 15:11:40 +0100

    Knight possibleMoves

--- ChessApp/Pieces/Knight.cs
+++ ChessApp/Pieces/Knight.cs
@@ -13,7 +13,24 @@ namespace ChessApp
        
         public override List<BoardSquare> ListPossibleMoves(ILocation location, List<List<BoardSquare>> board)
         {
-            return null;
+            BoardSquare move(int changeX, int changeY)
+            {
+                return board[location.y + changeY][location.x + changeX];
+            }
+
+            var possibilities = new List<BoardSquare>();
+
+            possibilities.Add(move(1, -2));
+            possibilities.Add(move(2, -1));
+            possibilities.Add(move(2, 1));
+            possibilities.Add(move(1, 2));
+            possibilities.Add(move(-1, 2));
+            possibilities.Add(move(-2, 1));
+            possibilities.Add(move(-2, -1));
+            possibilities.Add(move(-1, -2));
+
+
+            return possibilities;
         }
     }
 }
